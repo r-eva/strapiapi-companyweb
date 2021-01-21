8e6c89a602ab97abcdf05370a508ec83a08c1025
regina <reginaevadewi@gmail.com>
2021-01-21 21:47:57 +0100

    initial files commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1abe00208b721d8b470fa21f8b1ad434'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ca2eed92-acdd-41ee-ab11-b0c140f7c54e'
+};
